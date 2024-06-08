e5003e041165ff9ece20b50873d423af2f694b16
SheilamaeF <smae39591@gmail.com>
2024-06-08 18:18:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const commentbox = document.getElementById("commentbox")
 function submitComment(){
 
 commentbox.textContent += comment.value.toString() + "\n"
-
+comment.value=""
 }
 
 submit.addEventListener("click", submitComment)
